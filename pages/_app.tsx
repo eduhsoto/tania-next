e5117fb0105db80b09d516409ab56bf006b65e9d
Eduardo Hernández Soto <hseduardo@gmail.com>
2023-03-04 22:59:46 -0600

    hco component protec routes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,20 @@
 import Footer from '@/components/Footer'
 import NavBar from '@/components/navbar/NavBar'
+import ProctedRoute from '@/components/ProctedRoute'
 import { AuthProvider } from '@/context/authContext'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import type { ReactNode } from 'react'
+import AddItem from './additem'
+import Dashboard from './dashboard'
+import EditItem from './edit/[id]'
+
+const DefaultComponent = ({children} : { children: ReactNode }): React.ReactElement => <>{children}</>
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  const Auth = Component === Dashboard || Component === AddItem || Component === EditItem
+  const IsComponent = Auth ? ProctedRoute : DefaultComponent
   return (
     <>
       <Head>
@@ -17,7 +26,9 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
       </Head>
       <AuthProvider>
         <NavBar />
+        <IsComponent>
         <Component {...pageProps} />
+        </IsComponent>
         <Footer />
       </AuthProvider>
     </>
